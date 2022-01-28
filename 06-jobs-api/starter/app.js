a618efaf6b7b609e473f68c5dc2e2fde3ce067bf
JosepAnSabaté <josep.andreu@e-campus.uab.cat>
2022-01-28 18:30:37 +0100

    jobstask

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// secutity packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+
+
+
 const express = require('express');
 const app = express();
 const connectDB  = require('./db/connect')
@@ -7,16 +17,26 @@ const connectDB  = require('./db/connect')
 const authRouter = require('./routes/auth')
 const jobsRouter = require('./routes/jobs')
 const authenticatedUser = require('./middleware/authentication')
+
+app.use(rateLimiter({
+  windowMS: 15*60*1000,//15minuts
+  max: 100//100 req per window
+}))
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(rateLimiter());
+
 
 // routes
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs',authenticatedUser, jobsRouter)
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs',authenticatedUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -10,13 +10,16 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg:err.message || 'Something went wrong try again later'
   }
 
-
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors).map((item)=>item.message).join(',')
+    customError.statusCode = 400;
   }
   if (err.code && err.code == 11000) {
     customError.msg = `Duplicate value for ${Object.keys(err.keyValue)} field, please choose another value`
-    customError.statusCode = 400
+    customError.statusCode = 400;
   }
   return res.status(customError.statusCode).json({ msg: customError.msg })
   //return res.status(customError.statusCode).json({ err })
